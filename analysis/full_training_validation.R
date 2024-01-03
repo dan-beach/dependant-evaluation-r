b48c5cd119cc68c25d6b71a944aff23ead078915
Dan Beach <danbeach@btinternet.com>
2024-01-03 13:27:28 +0000

    - Added set.seed(0) - Added new metrics (prec, f1, npv, pr_auc) - Added PR curve calculation - Added PR plot for cell line full test set - Added ROC and PR plots for tests filtered by dep gene frequency - Data saved for all plots - New metrics added to all saved data files - Changed the way metrics are added to vector (metrics order was reversed previously)

--- analysis/full_training_validation.R
+++ analysis/full_training_validation.R
@@ -1,5 +1,6 @@
-#tags
-#tag='raw_0'
+#tag='base'
+
+set.seed(0)
 
 # Initialize a dataframe with cols for feature names 
 training = data.frame("gene"=character(), "betweenness"=numeric(), "constraint"=numeric(), "closeness"=numeric(), "coreness"=numeric(), "degree"=numeric(), "eccentricity"=numeric(), "eigen_centrality"=numeric(),  "hub_score"=numeric(), "neighborhood1.size"=numeric(), "neighborhood2.size"=numeric(), "neighborhood6.size"=numeric(), "d6_to_d2_neighbours"=numeric(), "page_rank"=numeric(), "dependent"=factor())
@@ -24,16 +25,26 @@ print('Model being trained...')
 # Settings are same as those used in analysis.R and use AdaBoost classifier
 control <- trainControl(method="cv", summaryFunction=twoClassSummary, classProbs=T,
                         savePredictions = T)
+
 rf <- train(dependent ~ ., data=select(training, -gene), method='ada', trControl=control, tuneLength=5, metric="ROC", preProc=c("center", "scale"))
 
+# Initialise ROC metrics 
 auc <- c()
 acc <- c()
 spec <- c()
 sens <- c()
 
+# New metrics added
+prec <- c()
+f1 <- c()
+npv <- c()
+pr_auc <- c()
+
+
 # For each cell line in train_cell_lines get a list of genes from the testing data and assign to 'testing'
-# This is a validation of the model trained on the super traning set - using testing data form the train_cell_lines lines 
+# This is a validation of the model trained on the super traning set - using testing data from the train_cell_lines lines 
 for(test_cell_line in train_cell_lines) {
+  
   print('validation for:')
   print(test_cell_line)
   testing <- read.table(sprintf('%s/training/%s_testing_%s.csv', data_dir, test_cell_line, tag), sep='\t', header = T)
@@ -50,32 +61,147 @@ for(test_cell_line in train_cell_lines) {
     mutate(pred = case_when(dependent > 0.5 ~ 'dependent', 
                             TRUE ~ 'non_dependent'))
   
+  # some debugging
+  print('prc first round')
+  print(validation$dependent)
+  print(testing$dependent == "dependent")
+  print(test_cell_line)
+  print('end of debug printout')
+  
   # Generate confusion matrix and roc curve
   cm <- confusionMatrix(as.factor(testing$dependent), as.factor(validation$pred))
   roc <- roc(testing$dependent, validation$dependent)
+  prc <- pr.curve(scores.class0 = validation$dependent, weights.class0=testing$dependent == "dependent", curve=T)
   
-  # save metrics 
-  auc <- c(roc$auc, auc)
-  acc <- c(cm$overall['Accuracy'], acc)
-  spec <- c(cm$byClass['Specificity'], spec)
-  sens <- c(cm$byClass['Sensitivity'], sens)
+  # Get values of acc, sens, spec, prec, f1, npv, pr_auc. 
+  # Acc, sens and spec are are named vectors and won't behave corretcly in the calcs below (esp f1)
+  auc_value <- roc$auc
+  acc_value <- as.numeric(cm$overall['Accuracy'])
+  spec_value <- as.numeric(cm$byClass['Specificity'])
+  sens_value <- as.numeric(cm$byClass['Sensitivity'])
+  prec_value <- posPredValue(as.factor(validation$pred), as.factor(testing$dependent))
+  f1_value <- 2 * (prec_value * sens_value) / (prec_value + sens_value)
+  npv_value <- negPredValue(as.factor(validation$pred), as.factor(testing$dependent))
+  pr_auc_value <- prc$auc.integral
   
-  # generate ROC plot
-  # save to results/{cell_line}_super_roc_{tag}.pdf
+  # Save metrics
+  # auc <- c(auc_value, auc)
+  # acc <- c(acc_value, acc)
+  # spec <- c(spec_value, spec)
+  # sens <- c(sens_value, sens)
+  # prec <- c(prec_value, prec)
+  # f1 <- c(f1_value, f1)
+  # npv <- c(npv_value, npv)
+  # pr_auc <- c(pr_auc_value, pr_auc)
+  
+  auc <- c(auc, auc_value)
+  acc <- c(acc, acc_value)
+  spec <- c(spec, spec_value)
+  sens <- c(sens, sens_value)
+  prec <- c(prec, prec_value)
+  f1 <- c(f1, f1_value)
+  npv <- c(npv, npv_value)
+  pr_auc <- c(pr_auc, pr_auc_value)
+  
+  # auc
+  # acc
+  # spec
+  # sens
+  # prec
+  # f1
+  # npv
+  # pr_auc
+  
+  #########
+  # Generate plots for ROC and PR and save to file with data
+  #########
+  
+  # Define the interval for breaks
+  # Used for both plots
   interval = 0.2
   breaks = seq(0, 1, interval)
+  
+  #####
+  # ROC
+  #####
+  
+  # Extract sens and spec values
+  specificity_vals <- roc$specificities
+  sensitivity_vals <- roc$sensitivities
+  
+  # Creating a dataframe for ggplot
+  roc_data <- data.frame(Specificity = specificity_vals, Sensitivity = sensitivity_vals)
+  
   roc_plot <- ggplot() +
     geom_segment(aes(x = 0, y = 1, xend = 1,yend = 0), alpha = 0.5) +
     geom_step() +
-    geom_path(aes(x=roc$specificities, y=roc$sensitivities)) +
+    geom_path(aes(x=specificity_vals, y=sensitivity_vals)) +
     scale_x_reverse(name = "Specificity",limits = c(1,0), breaks = breaks, expand = c(0.001,0.001)) +
     scale_y_continuous(name = "Sensitivity", limits = c(0,1), breaks = breaks, expand = c(0.001, 0.001)) +
     ggtitle(sprintf('Dependency validation')) +
-    annotate("text", x = interval/2 + 0.1, y = interval/2, vjust = 0, label = paste("AUC \t",sprintf("%.3f",roc$auc)))
+    annotate("text", x = interval/2 + 0.1, y = interval/2, vjust = 0, label = paste("AUC: ",sprintf("%.3f",auc_value)))
   
-  plot(roc_plot)
-  print(sprintf('saving %s', test_cell_line ))
-  ggsave(sprintf('%s/results/%s_super_roc_%s.pdf', data_dir, test_cell_line, tag), roc_plot)
+  print(roc_plot)
+  
+  #####
+  # PR
+  #####
+  
+  # Extracting PR curve data
+  pr_curve_data <- prc$curve
+  
+  # Assuming the first column is Recall, the second column is Precision
+  recall_vals <- pr_curve_data[, 1]
+  precision_vals <- pr_curve_data[, 2]
+  
+  # Creating a dataframe for ggplot
+  pr_data <- data.frame(Recall = recall_vals, Precision = precision_vals)
+  
+  # Creating the PR plot using ggplot2
+  pr_plot <- ggplot(pr_data, aes(x = Recall, y = Precision)) +
+    geom_path() +
+    scale_x_continuous(name = "Recall", limits = c(0, 1), breaks = breaks, expand = c(0.001, 0.001)) +
+    scale_y_continuous(name = "Precision", limits = c(0, 1), breaks = breaks, expand = c(0.001, 0.001)) +
+    ggtitle('Precision-Recall Curve') +
+    annotate("text", x = interval/2 + 0.1, y = interval/2, vjust = 0, label = paste("AUC: ",sprintf("%.3f", pr_auc_value))) +
+    theme_minimal()
+  
+  # Display the plot
+  print(pr_plot)
+  
+  #####
+  # Save the plots and data
+  #####
+  
+  print(sprintf('saving %s plots', test_cell_line ))
+  
+  # Save ROC plot and data
+  ggsave(sprintf('%s/results/plots/%s_super_roc_%s.pdf', data_dir, test_cell_line, tag), roc_plot)
+  write.csv(roc_data, sprintf("%s/results/plot_data/%s_super_roc_%s_data.csv", data_dir, test_cell_line, tag), row.names = FALSE)
+  
+  # Save PR plot and data
+  ggsave(sprintf('%s/results/plots/%s_super_pr_%s.pdf', data_dir, test_cell_line, tag), pr_plot)
+  write.csv(pr_data, sprintf("%s/results/plot_data/%s_super_pr_%s_data.csv", data_dir, test_cell_line, tag), row.names = FALSE)
+
+  
+  # # Original ROC plot code here
+  # # generate ROC plot
+  # # save to results/{cell_line}_super_roc_{tag}.pdf
+  # interval = 0.2
+  # breaks = seq(0, 1, interval)
+  # roc_plot <- ggplot() +
+  #   geom_segment(aes(x = 0, y = 1, xend = 1,yend = 0), alpha = 0.5) +
+  #   geom_step() +
+  #   geom_path(aes(x=roc$specificities, y=roc$sensitivities)) +
+  #   scale_x_reverse(name = "Specificity",limits = c(1,0), breaks = breaks, expand = c(0.001,0.001)) +
+  #   scale_y_continuous(name = "Sensitivity", limits = c(0,1), breaks = breaks, expand = c(0.001, 0.001)) +
+  #   ggtitle(sprintf('Dependency validation')) +
+  #   annotate("text", x = interval/2 + 0.1, y = interval/2, vjust = 0, label = paste("AUC \t",sprintf("%.3f",roc$auc)))
+  # 
+  # plot(roc_plot)
+  # print(sprintf('saving %s', test_cell_line ))
+  # ggsave(sprintf('%s/results/%s_super_roc_%s.pdf', data_dir, test_cell_line, tag), roc_plot)
+
 }
 
 # Save results to file 
@@ -84,12 +210,10 @@ print(mean(auc))
 print(data.frame(train_cell_lines, auc))
 auc
 train_cell_lines
-write.table(data.frame('cl'=train_cell_lines, 'auc'=auc, 'acc'=acc, 'sens'=sens, 'spec'=spec), sprintf('%s/results/super_rocs%s.csv', data_dir, tag), row.names=F, sep=',')
-
+write.table(data.frame('cl'=train_cell_lines, 'auc'=auc, 'acc'=acc, 'sens'=sens, 'spec'=spec, 'prec'=prec, 'f1'=f1, 'npv'=npv, 'pr_auc'=pr_auc), sprintf('%s/results/super_rocs_%s.csv', data_dir, tag), row.names=F, sep=',')
 
 
 #####   Grades of cell line specific genes  
-
 # Repeat the analysis to predict subset of genes that are dependent in 1, 10 20, 30 39 cell lines
 # Does not filter genes out of test set if they are also in the training set
 
@@ -104,7 +228,13 @@ for(cell_line_count in c(counts)) {
   acc <- c()
   spec <- c()
   sens <- c()
-  
+
+  # New metrics added
+  prec <- c()
+  f1 <- c()
+  npv <- c()
+  pr_auc <- c()
+    
   for(test_cell_line in train_cell_lines) {
     
     # Filter for genes in a certain number of cell lines
@@ -140,41 +270,185 @@ for(cell_line_count in c(counts)) {
     # plot the ROC curve and save to /results/{cell_line}_super_roc_{tag}.pdf
     
     if (length(unique(as.factor(validation$pred))) == 2 & length(unique(as.factor(testing$dependent))) == 2) {
-    
+      
+      # some debugging
+      print('prc second round')
+      print(validation$dependent)
+      print(testing$dependent == "dependent")
+      print(test_cell_line)
+      print('end of debug printout')
+      
       cm <- confusionMatrix(as.factor(testing$dependent), as.factor(validation$pred))
       roc <- roc(testing$dependent, validation$dependent)
+      prc <- pr.curve(scores.class0 = validation$dependent, weights.class0=testing$dependent == "dependent", curve=T)
       
-      auc <- c(roc$auc, auc)
-      acc <- c(cm$overall['Accuracy'], acc)
-      spec <- c(cm$byClass['Specificity'], spec)
-      sens <- c(cm$byClass['Sensitivity'], sens)
+      # Get values of acc, sens, spec, prec, f1, npv, pr_auc. 
+      # Acc, sens and spec are are named vectors and won't behave corretcly in the calcs below (esp f1)
+      auc_value <- roc$auc
+      acc_value <- as.numeric(cm$overall['Accuracy'])
+      spec_value <- as.numeric(cm$byClass['Specificity'])
+      sens_value <- as.numeric(cm$byClass['Sensitivity'])
+      prec_value <- posPredValue(as.factor(validation$pred), as.factor(testing$dependent))
+      f1_value <- 2 * (prec_value * sens_value) / (prec_value + sens_value)
+      npv_value <- negPredValue(as.factor(validation$pred), as.factor(testing$dependent))
+      pr_auc_value <- prc$auc.integral
       
+      # Save metrics
+      auc <- c(auc, auc_value)
+      acc <- c(acc, acc_value)
+      spec <- c(spec, spec_value)
+      sens <- c(sens, sens_value)
+      prec <- c(prec, prec_value)
+      f1 <- c(f1, f1_value)
+      npv <- c(npv, npv_value)
+      pr_auc <- c(pr_auc, pr_auc_value)
+      
+      # auc
+      # acc
+      # spec
+      # sens
+      # prec
+      # f1
+      # npv
+      # pr_auc
+
+      # sens
+      # sens_value
+      # prec
+      # f1
+      # 
+      # sens <- c()
+      # sens_value <- c()
+      # prec <- c()
+      # f1 <- c()
+      
+      #########
+      # Generate plots for ROC and PR and save to file with data
+      #########
+      
+      # Define the interval for breaks
+      # Used for both plots
       interval = 0.2
       breaks = seq(0, 1, interval)
+      
+      #####
+      # ROC
+      #####
+      
+      # Extract sens and spec values
+      specificity_vals <- roc$specificities
+      sensitivity_vals <- roc$sensitivities
+      
+      # Creating a dataframe for ggplot
+      roc_data <- data.frame(Specificity = specificity_vals, Sensitivity = sensitivity_vals)
+      
       roc_plot <- ggplot() +
         geom_segment(aes(x = 0, y = 1, xend = 1,yend = 0), alpha = 0.5) +
         geom_step() +
-        geom_path(aes(x=roc$specificities, y=roc$sensitivities)) +
+        geom_path(aes(x=specificity_vals, y=sensitivity_vals)) +
         scale_x_reverse(name = "Specificity",limits = c(1,0), breaks = breaks, expand = c(0.001,0.001)) +
         scale_y_continuous(name = "Sensitivity", limits = c(0,1), breaks = breaks, expand = c(0.001, 0.001)) +
         ggtitle(sprintf('Dependency validation')) +
-        annotate("text", x = interval/2 + 0.1, y = interval/2, vjust = 0, label = paste("AUC \t",sprintf("%.3f",roc$auc)))
+        annotate("text", x = interval/2 + 0.1, y = interval/2, vjust = 0, label = paste("AUC: ",sprintf("%.3f",auc_value)))
       
-      plot(roc_plot)
-      print(sprintf('saving %s', test_cell_line ))
-      ggsave(sprintf('%s/results/%s_super_roc_%s.pdf', data_dir, test_cell_line, tag), roc_plot)
+      print(roc_plot)
+      
+      #####
+      # PR
+      #####
+      
+      # Extracting PR curve data
+      pr_curve_data <- prc$curve
+      
+      # Assuming the first column is Recall, the second column is Precision
+      recall_vals <- pr_curve_data[, 1]
+      precision_vals <- pr_curve_data[, 2]
+      
+      # Creating a dataframe for ggplot
+      pr_data <- data.frame(Recall = recall_vals, Precision = precision_vals)
+      
+      # Creating the PR plot using ggplot2
+      pr_plot <- ggplot(pr_data, aes(x = Recall, y = Precision)) +
+        geom_path() +
+        scale_x_continuous(name = "Recall", limits = c(0, 1), breaks = breaks, expand = c(0.001, 0.001)) +
+        scale_y_continuous(name = "Precision", limits = c(0, 1), breaks = breaks, expand = c(0.001, 0.001)) +
+        ggtitle('Precision-Recall Curve') +
+        annotate("text", x = interval/2 + 0.1, y = interval/2, vjust = 0, label = paste("AUC: ",sprintf("%.3f", pr_auc_value))) +
+        theme_minimal()
+      
+      # Display the plot
+      print(pr_plot)
+      
+      #####
+      # Save the plots and data
+      #####
+      
+      print(sprintf('saving %s plots', test_cell_line ))
+      
+      # Save ROC plot and data
+      ggsave(sprintf('%s/results/plots/%s_super_roc_%s_%s.pdf', data_dir, test_cell_line, tag, cell_line_count), roc_plot)
+      write.csv(roc_data, sprintf("%s/results/plot_data/%s_super_roc_%s_%s_data.csv", data_dir, test_cell_line, tag, cell_line_count), row.names = FALSE)
+      
+      # Save PR plot and data
+      ggsave(sprintf('%s/results/plots/%s_super_pr_%s_%s.pdf', data_dir, test_cell_line, tag, cell_line_count), pr_plot)
+      write.csv(pr_data, sprintf("%s/results/plot_data/%s_super_pr_%s_%s_data.csv", data_dir, test_cell_line, tag, cell_line_count), row.names = FALSE)
+  
+            
+      # # Original plot code below
+      # 
+      # interval = 0.2
+      # breaks = seq(0, 1, interval)
+      # roc_plot <- ggplot() +
+      #   geom_segment(aes(x = 0, y = 1, xend = 1,yend = 0), alpha = 0.5) +
+      #   geom_step() +
+      #   geom_path(aes(x=roc$specificities, y=roc$sensitivities)) +
+      #   scale_x_reverse(name = "Specificity",limits = c(1,0), breaks = breaks, expand = c(0.001,0.001)) +
+      #   scale_y_continuous(name = "Sensitivity", limits = c(0,1), breaks = breaks, expand = c(0.001, 0.001)) +
+      #   ggtitle(sprintf('Dependency validation')) +
+      #   annotate("text", x = interval/2 + 0.1, y = interval/2, vjust = 0, label = paste("AUC \t",sprintf("%.3f",roc$auc)))
+      # 
+      # plot(roc_plot)
+      # print(sprintf('saving %s', test_cell_line ))
+      # ggsave(sprintf('%s/results/%s_super_roc_%s_%s.pdf', data_dir, test_cell_line, tag, cell_line_count), roc_plot)
+    
     } else {
       
-      auc <- c(0.5, auc)
-      acc <- c(0.5, acc)
-      spec <- c(1, spec)
-      sens <- c(0, sens)
+      auc <- c(auc, 0.5)
+      acc <- c(acc, 0.5)
+      spec <- c(spec, 1)
+      sens <- c(sens, 0)
+      prec <- c(prec, 0.5)
+      f1 <- c(f1, 0.5)
+      npv <- c(npv, 0.5)
+      pr_auc <- c(pr_auc, 0)
+      
     }
+    
+    print(train_cell_lines)
+    print(auc)
+    print(acc)
+    print(spec)
+    print(sens)
+    print(prec)
+    print(f1)
+    print(npv)
+    print(pr_auc)
   } 
   
   ### 
-  print(cell_line_count)
-  super_rocs <- data.frame('cl'=train_cell_lines , 'auc'=auc, 'acc'=acc, 'sens'=sens, 'spec'=spec)
+  #print(cell_line_count)
+  
+  print(train_cell_lines)
+  print(auc)
+  print(acc)
+  print(spec)
+  print(sens)
+  print(prec)
+  print(f1)
+  print(npv)
+  print(pr_auc)
+  
+  super_rocs <- data.frame('cl'=train_cell_lines , 'auc'=auc, 'acc'=acc, 'sens'=sens, 'spec'=spec, 'prec'=prec, 'f1'=f1, 'npv'=npv, 'pr_auc'=pr_auc)
   print(super_rocs)
   write.table(super_rocs, sprintf('%s/results/cell_line_results/super_rocs_cls_%s_%s.csv', data_dir, tag, cell_line_count), row.names=F, sep=',')
   print('Done!')
