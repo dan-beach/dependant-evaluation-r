2c737f91d8f4bb17e5f083a7c25582d9e6b83493
Dan Beach <danbeach@btinternet.com>
2024-01-03 12:49:11 +0000

    - Added PPROC - Updated tags and counts for new pieline - Added new source file for process_dependencies_23q4.R - Commented out gene ID mapping scripts - Updated reference to Reactome base PPI. - Commented out concat_results and plot_results scripts for testing

--- multi_pipe.R
+++ multi_pipe.R
@@ -8,6 +8,7 @@ library(caret)
 library(mlbench)
 library(plotROC)
 library(pROC)
+library(PRROC)
 library(doMC)
 registerDoMC(cores = 4)
 
@@ -21,7 +22,7 @@ registerDoMC(cores = 4)
 
 #tags <- c('reactome_base', 'reactome_0', 'reactome_1')
 
-tags <- c('reactome_1')
+tags <- c('base', 'test_x')
 
 #tags <- c('base', 'raw_0', 'raw_1')
 
@@ -32,22 +33,23 @@ tags <- c('reactome_1')
           # 'raw_4', 'prop_tt_4', 'prop_all_4',
           # 'base')
 
-counts = c(1, 10, 20, 30, 39)
+counts = c(1, 10, 20, 30, 37)
+#counts = c(1, 5)
 
 print('Load files for feature processing')
-#data_dir <- '/its/home/gb293/phd/data/slant_cancer'
+
 data_dir <- 'data/reactome_23q4/'
 
-# read genes to proteins ID map and store as id_map
-# rename the columns
-id_map <- read.table(sprintf('%s/supporting_files/human_id_map.txt', data_dir), header=TRUE, sep='\t', fill=T, comment.char = '', quote = '', stringsAsFactors = F)
-names(id_map) <- c('Gene.ID', 'Transcript.ID',	'Protein.ID',	'Associated.Gene.Name',	'External.ID')
+# # Read genes to proteins ID map and store as id_map
+# # Not needed for Reactome. It adds a gene_id column to the dependencies dataframe and about 2 million extra records that are not needed for Reactome
+# id_map <- read.table(sprintf('%s/supporting_files/human_id_map.txt', data_dir), header=TRUE, sep='\t', fill=T, comment.char = '', quote = '', stringsAsFactors = F)
+# names(id_map) <- c('Gene.ID', 'Transcript.ID',	'Protein.ID',	'Associated.Gene.Name',	'External.ID')
 
 ### Each source is a one off process, once these have been run you can use the read script below
-source('preprocessing/process_dependencies.R')
+source('preprocessing/process_dependencies_23q4.R')
 dependencies <- read.table(sprintf('%s/dependency/processed/dependencies.csv', data_dir), stringsAsFactors = F, sep=' ')
 
-# create the training/testing cell lines list
+# Create the training/testing cell lines list
 source('preprocessing/create_cl_list.R')
 
 # DRB Read contents of cl_list into cell lines
@@ -59,11 +61,11 @@ train_cell_lines <- cell_lines %>% filter(train==1) %>% select(cl) %>% unlist() 
 test_cell_lines <- cell_lines %>% filter(train==0) %>% select(cl) %>% unlist() %>% as.vector()
 
 # Process the base PPI
+# PPI has been pre-processed so comment this line out
 #source('preprocessing/process_human_ppi.R')
 
 # Load the processed PPI as interactions
-#interactions <- read.csv(sprintf('%s/interactions/processed/base_ppi.csv', data_dir), stringsAsFactors = F)
-interactions <- read.csv(sprintf('%s/interactions/processed/reactome_ppi.csv', data_dir), stringsAsFactors = F)
+interactions <- read.csv(sprintf('%s/interactions/processed/reactome_base_ppi_v1.csv', data_dir), stringsAsFactors = F)
 
 ### Remove x% of PPIN
 # This is only done to test how robust the models are to incomplete ppi networks
@@ -72,19 +74,17 @@ rows_to_keep = nrow(interactions) * 1
 interactions <- interactions[sample(1:nrow(interactions), rows_to_keep, replace=FALSE),]
 print(str(interactions))
 
-# calculate the frequency of dependent genes in each cell line set
+# Calculate the frequency of dependent genes in each cell line set
 # df contains a column if all genes in the PPI and a count of how many of the training cell lines they are essential in
-# this seems to generate standalone meta data and not involved in rest of pipeline?
 source('preprocessing/dependency_freq.R')
-
 
 for(tag in tags) {
  print(sprintf('###############################%s', tag))
  source('pipeline.R')
- source('analysis/other_analysis/concat_results.R')
+ #source('analysis/other_analysis/concat_results.R')
 }
 
 print('Concatenate results')
-source('analysis/other_analysis/plot_results.R')
+#source('analysis/other_analysis/plot_results.R')
 
 source('analysis/full_training_validation.R')
